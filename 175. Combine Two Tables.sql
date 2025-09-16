2887635912b1ab3a7d93d83ef85623cb8e824ca1
unknown <noguchih@TDK-ZENSY.local>
2025-09-16 15:14:14 +0900

    commit

---
+++ 175. Combine Two Tables.sql
@@ -0,0 +1,1 @@
+SELECT p.firstname, p.lastname, a.city, a.state FROM Person AS p LEFT JOIN Address AS a ON p.personId = a.personId;
--- 350. Intersection of Two Arrays II.ts
+++ 350. Intersection of Two Arrays II.ts
@@ -1,4 +1,20 @@
-function intersect(nums1: number[], nums2: number[]): number[] {}
+function intersect(nums1: number[], nums2: number[]): number[] {
+  const result: number[] = [];
+  const counts: Record<string, number> = {};
+
+  for (let num of nums1) {
+    counts[num] = (counts[num] ?? 0) + 1;
+  }
+
+  for (let num of nums2) {
+    if (counts[num]) {
+      result.push(num);
+      counts[num]--;
+    }
+  }
+
+  return result;
+}
 
 console.log(intersect([1, 2, 2, 1], [2, 2]));
 console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
